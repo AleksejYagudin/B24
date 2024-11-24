699b1f1a2633d8e29942071cd315f610cd17373f
AleksejYagudin <49141634+AleksejYagudin@users.noreply.github.com>
2024-11-24 15:29:52 +0400

    AY/hw25_fix

--- custom-communication/index.php
+++ custom-communication/index.php
@@ -21,8 +21,10 @@ $response = json_decode($responseRaw, true);
 
 //ID Контакта
 $contactId = $response['result']['OWNER_ID'];
+$log = print_r($response, true);
+file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
 
-if($response['result']['PROVIDER_ID'] == 'CRM_EMAIL' && $contactId > 0) {
+if(($response['result']['PROVIDER_ID'] == 'CRM_EMAIL' || $response['result']['PROVIDER_ID'] == 'CRM_CALL') && $contactId > 0) {
     $url = 'https://b24loc.ru/rest/1/abgs8vpo71ts1jc2/crm.contact.update.json';
     $curDate = (new \DateTimeImmutable())->format('d.m.Y H:i:s');
     $data = ['ID' => $contactId, 'FIELDS[UF_CUSTOM_DATE_TIME_EMAIL]' => $curDate];
