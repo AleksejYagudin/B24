7cc6f70a26bb2c5a9ba78bf6b249e51c6cb6877f
AleksejYagudin <49141634+AleksejYagudin@users.noreply.github.com>
2024-11-24 13:36:13 +0400

    AY/hw25

---
+++ custom-communication/index.php
@@ -0,0 +1,44 @@
+<?php
+
+//ID Дела
+$activityId = $_REQUEST['data']['FIELDS']['ID'];
+
+$url = 'https://b24loc.ru/rest/1/6s58qwd2ud7lxgi2/crm.activity.get.json';
+$data = ['ID' => $activityId];
+
+$options = [
+    'http' => [
+        'method' => 'POST',
+        'header' => 'Content-type: application/x-www-form-urlencoded',
+        'content' => http_build_query($data),
+    ],
+];
+
+$context = stream_context_create($options);
+
+$responseRaw = file_get_contents($url, false, $context);
+$response = json_decode($responseRaw, true);
+
+//ID Контакта
+$contactId = $response['result']['OWNER_ID'];
+
+if($response['result']['PROVIDER_ID'] == 'CRM_EMAIL' && $contactId > 0) {
+    $url = 'https://b24loc.ru/rest/1/abgs8vpo71ts1jc2/crm.contact.update.json';
+    $curDate = (new \DateTimeImmutable())->format('d.m.Y H:i:s');
+    $data = ['ID' => $contactId, 'FIELDS[UF_CUSTOM_DATE_TIME_EMAIL]' => $curDate];
+
+    $options = [
+        'http' => [
+            'method' => 'POST',
+            'header' => 'Content-type: application/x-www-form-urlencoded',
+            'content' => http_build_query($data),
+        ],
+    ];
+
+    $context = stream_context_create($options);
+
+    $responseRaw = file_get_contents($url, false, $context);
+    $response = json_decode($responseRaw, true);
+}
+?>
+
---
+++ custom-communication/settings.php
@@ -0,0 +1,11 @@
+<?php
+define('C_REST_CLIENT_ID','local.6742c14d195483.47669863');//Application ID
+define('C_REST_CLIENT_SECRET','1hVWVoX3oEfBVug5U2A7BcROHUxL7bohCa3wvigPldb8QlA7BA');//Application key
+// or
+//define('C_REST_WEB_HOOK_URL','https://b24loc.ru/rest/1/6s58qwd2ud7lxgi2/');//url on creat Webhook
+
+//define('C_REST_CURRENT_ENCODING','windows-1251');
+//define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
+//define('C_REST_LOG_TYPE_DUMP',true); //logs save var_export for viewing convenience
+//define('C_REST_BLOCK_LOG',true);//turn off default logs
+//define('C_REST_LOGS_DIR', __DIR__ .'/logs/'); //directory path to save the log
